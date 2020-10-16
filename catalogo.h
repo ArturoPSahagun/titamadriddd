30223993ae1e11a943b4c8586f14df222cf4bab4
Arturo Sahagun <arturo.bernal3648@alumnos.udg.mx>
2020-10-16 16:49:22 -0500

    Función de entrada y generación de llaves.

---
+++ catalogo.h
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <sstream>
+
+
+using namespace std;
+
+class catalogo{
+private:
+    string bufferNombre;
+    string bufferDesc;
+public:
+    string hazLlave();
+    void capturar();
+};
+
+void catalogo::capturar(){
+    cout << "Ingresar nombre: " << endl;
+    getline(cin, bufferNombre);
+    cout << "Ingresar descripcion: " << endl;
+    getline(cin, bufferDesc);
+    string llave = hazLlave();
+    ofstream archivo("catalogo.txt");
+    archivo << llave.length() << llave << bufferNombre.length() << bufferNombre << bufferDesc.length() << bufferDesc << "\n";
+    archivo.close();
+}
+
+string catalogo::hazLlave(){
+    stringstream llave;
+    srand(time(nullptr));
+    int numeroAleatorio = rand() % 100;
+    llave << (char)toupper(bufferNombre[0]) << (char) toupper(bufferNombre[1]) << (char) toupper(bufferNombre[2]) << "-";
+    if(numeroAleatorio < 10) llave << "0";
+    llave << numeroAleatorio;
+    return llave.str();
+}
--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
-print("holas")
+#include <iostream>
+#include "catalogo.h"
+using namespace std;
+
+int main(){
+    catalogo cat;
+    cout << "\nbolsos" << endl;
+    cat.capturar();
+    return 0;
+}
